698d17e728df10af25d578c1ffec51199e7f6827
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-27 08:12:43 +0530

    3rd

---
+++ DsaQuestions/Ques.java
@@ -0,0 +1,9 @@
+// print the table of 2
+public class Ques {
+    public static void main(String[] args) {
+        int n =2;
+        for(int i = 1;i<11;i++){
+            System.out.println(n*i);
+        }
+    }
+}
