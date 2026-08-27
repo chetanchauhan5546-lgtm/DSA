70fd7177b4fda0848d019668e0ec00d21113354a
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-27 22:20:48 +0530

    7th

--- DsaQuestions/Patters.java
+++ DsaQuestions/Patters.java
@@ -1,9 +1,8 @@
-public class Patter {
+public class Patters {
     public static void main(String[] args) {
         int n = 4;
-        for(int i=1;i<=n;i++){
-            //inner loop
-            for(int j = 1;j<=i;j++){
+        for (int i = n;i>=1;i--){
+            for (int j = 1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println("");
