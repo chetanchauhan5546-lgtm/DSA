c96a92250168b7a9c0547b8bacf8438101d2ab4f
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-27 07:45:05 +0530

    1st p

---
+++ DsaQuestions/Loop.java
@@ -0,0 +1,22 @@
+// Print 0 to 10.
+public class Loop {
+    public static void main(String[] args) {
+        // for (int i=0;i<11;i++){  //i++ = add 1 in value of i.
+            //System.out.println(i);
+            //  System.out.print(i + " ");
+        //}
+        // int i = 0;
+        // while(i<11){
+        //     System.out.println(i);
+        //    // i = i+1;
+        //    i++;
+        // }
+       int i = 0;
+        do{
+          System.out.println(i);
+          i++;
+        }while(i<11);
+
+        
+    }
+}
