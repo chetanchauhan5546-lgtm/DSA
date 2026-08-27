4b8f0ba84e122a47c578cf71d3142150a62d5165
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-27 11:25:48 +0530

    3rd

--- DsaQuestions/Ques.java
+++ DsaQuestions/Ques.java
@@ -1,7 +1,13 @@
+import java.util.*;
 // print the table of 2
 public class Ques {
     public static void main(String[] args) {
-        int n =2;
+        // int n =2;
+        // for(int i = 1;i<11;i++){
+        //     System.out.println(n*i);
+        // }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         for(int i = 1;i<11;i++){
             System.out.println(n*i);
         }
