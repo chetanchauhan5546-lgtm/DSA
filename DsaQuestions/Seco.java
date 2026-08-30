0dd74dc42b53dae6a751d956353daf3f15babff2
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-30 18:01:13 +0530

    2nd

---
+++ DsaQuestions/Seco.java
@@ -0,0 +1,12 @@
+// floyt Trangle
+public class Seco {
+    public static void main(String[] args) {
+        int n = 15;
+        for(int i =1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
