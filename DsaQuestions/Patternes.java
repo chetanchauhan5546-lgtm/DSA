8e900628add18dcbc2875dcfd71ed93b2b4e3758
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-08-31 19:08:42 +0530

    2nd

--- DsaQuestions/Patternes.java
+++ DsaQuestions/Patternes.java
@@ -4,7 +4,7 @@ public class Patternes {
         int num= 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print( num );
+                System.out.print( num +" ");
                 num ++;
             }
             System.out.println( );
