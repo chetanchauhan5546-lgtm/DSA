4d0215299e9feeb590f96a50b384810f4d7a0562
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-02 00:15:54 +0530

    5th

--- DsaQuestions/DimondPattern.java
+++ DsaQuestions/DimondPattern.java
@@ -1,8 +1,8 @@
 public class DimondPattern {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         for(int i =1;i<=n;i++){
-            for(int j=1;j<=n-1;j++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=2*i-1;j++){
@@ -11,7 +11,7 @@ public class DimondPattern {
             System.out.println();
         }
         for(int i =n;i>=1;i--){
-            for(int j=1;j<=n-1;j++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=2*i-1;j++){
