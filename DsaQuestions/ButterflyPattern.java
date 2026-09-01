a414e5420675a3da90a83226a935491e1d4c9761
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-01 23:32:04 +0530

    Today's 1st

--- DsaQuestions/ButterflyPattern.java
+++ DsaQuestions/ButterflyPattern.java
@@ -17,7 +17,7 @@ public class ButterflyPattern {
         //for lower half purpose
         for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
-                System.err.print("*");
+                System.out.print("*");
             }
             int space = 2*(n-i);
             for(int j=1;j<=space;j++){
