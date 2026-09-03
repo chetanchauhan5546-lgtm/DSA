d0c2e785edc423b5a6fec98971a24d1db0d3536a
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-03 19:12:30 +0530

    1st

---
+++ DsaQuestions/Ques3.java
@@ -0,0 +1,16 @@
+public class Ques3 {
+      public static void main(String[] args) {
+        int[] marks = new int[4];
+        marks[0] = 90;
+        marks[1] = 91;
+        marks[2] = 93;
+        marks[3] = 95;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        // System.out.println(marks[3]);
+        for(int i =0;i<=4;i++){
+            System.out.println(marks[i]);
+        }
+      }   
+}
