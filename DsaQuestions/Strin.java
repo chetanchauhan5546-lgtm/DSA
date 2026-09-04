fdde344b6c03fe05b68e1a7b4056edc7228c4fdf
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-04 19:00:01 +0530

    3rd

---
+++ DsaQuestions/Strin.java
@@ -0,0 +1,8 @@
+public class Strin {
+    public static void main(String[] args) {
+        String firstName = "Radha ";
+        String secondName = " Krishna";
+        String fullName = firstName + secondName;
+        System.out.println(fullName);
+    }
+}
