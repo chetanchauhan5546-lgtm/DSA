fa9996d48fb24ab019c642ce39ac0bb0b52211d3
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-04 19:25:41 +0530

    5th

--- DsaQuestions/StringCompare.java
+++ DsaQuestions/StringCompare.java
@@ -5,10 +5,17 @@ public class StringCompare {
         System.out.println(name1.length());
         System.out.println(name2.length());
 
-        if(name1.compareTo(name2) == 0){
+        // if(name1.compareTo(name2) == 0){    // 'Compare to' use for large scale 
+        //     System.out.println("Both name length size is equal");
+        // }else{
+        //     System.out.println("Both name length size is not equal ");
+        // }
+        if(name1 == name2){    
             System.out.println("Both name length size is equal");
         }else{
             System.out.println("Both name length size is not equal ");
         }
+
+
     }
 }
