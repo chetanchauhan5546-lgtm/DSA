973b172900987c670030c56581c2b2101bd2701c
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-04 19:38:09 +0530

    5th

---
+++ DsaQuestions/StringSum.java
@@ -0,0 +1,7 @@
+public class StringSum {
+    public static void main(String[] args) {
+        String sentense = "SitaRamam";
+        String name = sentense.substring(4);
+        System.out.println(name);
+    }
+}
