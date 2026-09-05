4242f937009767d0834afe07e63eacb2f822e917
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-05 10:10:31 +0530

    1st

---
+++ DsaQuestions/Stringsb.java
@@ -0,0 +1,8 @@
+public class Stringsb {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Rose");
+        System.out.println(sb);
+        sb.insert(2, 'e');
+        System.out.println(sb);
+    }
+}
